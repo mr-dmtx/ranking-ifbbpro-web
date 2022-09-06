0052032ddf0c55ebd4da3c20a62d8528c03b060f
Daniel Mendoza <daniel.mendozateixeira@gmail.com>
2022-09-06 11:37:08 -0300

    ultima leitura do web scrapper

--- server.py
+++ server.py
@@ -52,17 +52,30 @@ def ExibirDesempenhoAtleta(nomeAtleta):
             inner join posicao p on p.posicao_categoria = c.categoria_id 
             where posicao_atleta_nome like '{}';
         """.format(nomeAtleta)
-    print(query)
     cursor = g.conn.execute(query)
     json = [{'nome': row[0], 'posicao': row[1], 'categoria': row[2], 'evento': row[3]}
             for row in cursor.fetchall()]
     return json
 
-@app.route("/atletas/<nome>")
-def getAtletaNome(nome):
-    return "Atletas"
+@app.route("/ultima-atualizacao")
+def ultimaAtualizacaoLeitura():
+    query = """
+        select data_atualizacao 
+        from atualizacao a 
+        order by data_atualizacao desc limit 1;
+    """
+
+    cursor = g.conn.execute(query)
+    json = [{"data": row[0]} for row in cursor.fetchall()]
+
+    return json
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 #servidor heroku
+
+#heroku login
+#git add .
+#git commit -m ""
+#git push heroku master
